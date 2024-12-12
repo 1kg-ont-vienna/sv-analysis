7324166036b2e381fcf316bfaecae8d85229f92d
Tobias Rausch <rauschtobi@gmail.com>
2024-12-12 14:03:21 +0100

    short-read comp

--- scripts/mei_statistics/stats.sh
+++ scripts/mei_statistics/stats.sh
@@ -10,11 +10,6 @@ then
     wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20210124.SV_Illumina_Integration/1KGP_3202.Illumina_ensemble_callset.freeze_V1.vcf.gz.tbi
 fi
 
-## Comparison to short-reads 
-bcftools view ${BASEDIR}/../release/giggles-genotyping/final-vcf.unphased.vcf.gz | grep -m 1 "^#CHROM" | cut -f 10- | tr '\t' '\n' > samples.tsv
-exit;
-
-
 ## Insertions
 TOTAL=0
 for TE in Alu L1 SVA
@@ -34,4 +29,28 @@ do
     TOTAL=`expr ${TOTAL} + ${COUNT}`
 done
 echo "Reference MEI" ${TOTAL}
+
+## Comparison to short-reads 
+bcftools view ${BASEDIR}/../release/giggles-genotyping/final-vcf.unphased.vcf.gz | grep -m 1 "^#CHROM" | cut -f 10- | tr '\t' '\n' > samples.tsv
+if [ ! -f ont.vcf.gz ]
+then
+    bcftools view -O b -o tmp.bcf --min-ac 1 -S samples.tsv ${BASEDIR}/../release/giggles-genotyping/final-vcf.unphased.vcf.gz chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX
+    bcftools index tmp.bcf
+    bcftools annotate -c INFO -a ${BASEDIR}/../release/svan-annotation/final-vcf.unphased.SVAN_1.3.vcf.gz tmp.bcf | bgzip > ont.vcf.gz
+    tabix ont.vcf.gz
+    rm tmp.bcf tmp.bcf.csi
+fi
+if [ ! -f nygc.vcf.gz ]
+then
+    bcftools view --min-ac 1 -S samples.tsv 1KGP_3202.Illumina_ensemble_callset.freeze_V1.vcf.gz chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX | bgzip > nygc.vcf.gz
+    tabix nygc.vcf.gz
+fi
+for TE in Alu L1 SVA
+do
+    ONT=`bcftools query -i 'STRLEN(REF)<STRLEN(ALT)' -f "%ITYPE_N\t%FAM_N\n" ${BASEDIR}/../release/svan-annotation/final-vcf.unphased.SVAN_1.3.vcf.gz | awk '$2=="'${TE}'"' | wc -l`
+    ALTTAG=`echo ${TE} | sed 's/Alu/<INS:ME:ALU>/' | sed 's/L1/<INS:ME:LINE1>/' | sed 's/SVA/<INS:ME:SVA>/'`
+    NYGC=`zcat nygc.vcf.gz | grep -v "^#" | cut -f 5 | grep -w "${ALTTAG}" | wc -l`
+    INC=`echo " ( ( ${ONT} - ${NYGC} )  / ${NYGC} ) "| bc -l`
+    echo "Comparison to short-reads:" ${TE} ${INC}
+done
 
